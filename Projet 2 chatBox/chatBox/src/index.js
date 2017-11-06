46328165f0b08d5502312284ac20788bcede4f67
Bilane A <billieabdiw@gmail.com>
2017-11-06 10:18:43 +0100

    using React-router

--- Projet 2 chatBox/chatBox/src/index.js
+++ Projet 2 chatBox/chatBox/src/index.js
@@ -4,10 +4,28 @@ import { render } from 'react-dom';
 //Components
 import Connexion from './components/Connexion';
 import App from './components/App';
+import NotFound from './components/NotFound';
+// Router
+import { BrowserRouter, Match, Miss } from 'react-router';
 //CSS
 import './index.css';
 
+//component stateless, pour juste afficher
+const Root = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={Connexion} />
+				<Match pattern="/pseudo/:pseudo" component={App} />
+				<Miss component={NotFound} />
+			</div>
+		</BrowserRouter>
+
+	)
+}
+
+
 render(
-	<App />,
+	<Root />,
 	document.getElementById('root')
 );
