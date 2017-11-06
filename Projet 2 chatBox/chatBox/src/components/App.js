8d0a72009c89c1b3ad6e9d4bc59b60498613a176
Bilane A <billieabdiw@gmail.com>
2017-11-06 12:20:16 +0100

    changing state

--- Projet 2 chatBox/chatBox/src/components/App.js
+++ Projet 2 chatBox/chatBox/src/components/App.js
@@ -5,6 +5,22 @@ import Formulaire from './Formulaire';
 import Message from './Message';
 
 class App extends React.Component{
+
+	state = {
+		messages:{}
+	}
+
+	addMessage = (message) =>{
+		//Copier le state
+		//{...this.state} -> recupérer le stat
+		const messages = {...this.state.messages};
+		//on ajoute le message avec une clé timestamp
+		const timestamp = Date.now();
+		messages[`message-${timestamp}`] = message;
+		//Mettre à jour le state
+		this.setState({ messages });
+	};
+
 	render () {
 		return (
 			<div className="box">
@@ -13,7 +29,7 @@ class App extends React.Component{
 						<Message pseudo={this.props.params.pseudo} />
 
 					</div>
-					<Formulaire />
+					<Formulaire addMessage={this.addMessage} />
 				</div>			
 			</div>
 		)
